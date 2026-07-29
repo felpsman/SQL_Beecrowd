25b6cde0482fec30c1783a58177f6b617dc4aab5
Felipe Lamy <82849597+felpsman@users.noreply.github.com>
2026-07-29 17:06:36 -0300

    Delete BeeCrowdSQL/jogoexemplo.sql

--- BeeCrowdSQL/jogoexemplo.sql
+++
@@ -1,4 +0,0 @@
-
-SELECT timesheet.badge_number,first_name,guns_issued FROM cops 
-JOIN timesheet ON cops.badge_number = timesheet.badge_number WHERE cops.security_level = 3 
-AND timesheet.checkout_time > 2207
